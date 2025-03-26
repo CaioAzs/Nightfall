7747cd8abfa2503df13bb9eb5f4b8fd1857ca3c2
Caio de Souza Conceição <souzascaio23cs@gmail.com>
2025-03-26 20:52:24 -0300

    Added flashlight tests and knife animation

--- Nightfall/Assets/Scripts/PlayerController.cs
+++ Nightfall/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private bool facingLeft;
+
+    public bool FacingLeft { get { return facingLeft; } set { facingLeft = value; } }
+
     [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private Transform spotlight; 
 
     private PlayerControls playerControls;
     private Vector2 movement;
@@ -12,46 +17,69 @@ public class PlayerController : MonoBehaviour
     private Animator myAnimator;
     private SpriteRenderer mySpriteRender;
 
-
-    private void Awake() {
+    private void Awake()
+    {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         mySpriteRender = GetComponent<SpriteRenderer>();
     }
 
-    private void OnEnable() {
+    private void OnEnable()
+    {
         playerControls.Enable();
     }
 
-    private void Update() {
+    private void Update()
+    {
         PlayerInput();
+        RotateSpotlight(); 
     }
 
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
         AdjustPlayerFacingDirection();
         Move();
     }
 
-    private void PlayerInput() {
+    private void PlayerInput()
+    {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
 
         myAnimator.SetFloat("moveX", movement.x);
         myAnimator.SetFloat("moveY", movement.y);
     }
 
-    private void Move() {
+    private void Move()
+    {
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
     }
 
-    private void AdjustPlayerFacingDirection() {
+    private void AdjustPlayerFacingDirection()
+    {
         Vector3 mousePos = Input.mousePosition;
         Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
 
-        if (mousePos.x < playerScreenPoint.x) {
+        if (mousePos.x < playerScreenPoint.x)
+        {
             mySpriteRender.flipX = true;
-        } else {
-            mySpriteRender.flipX = false;
+            FacingLeft = true;
         }
+        else
+        {
+            mySpriteRender.flipX = false;
+            FacingLeft = false;
+        }
+    }
+
+    private void RotateSpotlight()
+    {
+        if (spotlight == null) return;
+
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = (mouseWorldPosition - transform.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        spotlight.rotation = Quaternion.Euler(0, 0, angle);
     }
 }
